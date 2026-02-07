049b18fc61ad941b6b7d5e8bc5ef8a518e8ba623
qthien202 <nth4356@gmail.com>
2026-02-07 15:36:46 +0700

    Remove old single-app project structure
    Remove GetX controllers, views, widgets, platform folders (android,
    ios, linux, macos, web, windows), old assets and build scripts.

--- build_runner.sh
+++
@@ -1,2 +0,0 @@
-fvm flutter packages pub run build_runner build --delete-conflicting-outputs
-fvm flutter pub run flutter_launcher_icons:main
--- lib/app/controller/index/index_controller.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get/get.dart';
-
-class IndexController extends GetxController {
-  Rxn<int> tabIndex = Rxn();
-}
